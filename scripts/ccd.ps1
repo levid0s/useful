8e739f9a06bacdbf96c804ca73edccaf1b32a357
Lev <lev@me.com>
2023-04-15 17:48:35 +0100

    improved ccd.ps1

--- scripts/ccd.ps1
+++ scripts/ccd.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION      2023.03.22
+.VERSION      2023.04.15
 .AUTHOR       Levente Rog
 .COPYRIGHT    (c) 2023 Levente Rog
 .LICENSEURI   https://github.com/levid0s/useful/blob/master/LICENSE.md
@@ -14,14 +14,39 @@ Open a folder browser dialog and changes the current directory to the selected f
 ccd
 # do stuff...
 popd
+
+.EXAMPLE
+# Specify starting directory
+ccd d:\
+"d:\" | ccd
+
+# Don't change dir, just return the selected path
+$selectedPath = ccd -PassThru
+$selectedPath = get-item c:\ | ccd -PassThru
 #>
 
-[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-$foldername = New-Object System.Windows.Forms.FolderBrowserDialog
-$foldername.Description = "Select a folder"
-$foldername.rootfolder = "MyComputer"
-$foldername.SelectedPath = $pwd.Path
+[CmdletBinding()]
+param(
+  [Parameter(ValueFromPipeline = $true)][string]$Path,
+  [switch]$PassThru
+)
 
-if ($foldername.ShowDialog() -eq "OK") {
-  Push-Location $foldername.SelectedPath
+if (!$Path) {
+  $Path = $pwd.Path
 }
+
+[System.Reflection.Assembly]::LoadWithPartialName('System.windows.forms') | Out-Null
+$foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+$foldername.Description = 'Select a folder'
+$foldername.rootfolder = 'MyComputer'
+$foldername.SelectedPath = $Path
+
+if ($foldername.ShowDialog() -ne 'OK') {
+  return
+}
+
+if ($PassThru) {
+  return $foldername.SelectedPath
+}
+
+Push-Location $foldername.SelectedPath
